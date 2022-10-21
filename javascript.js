bc4b03747e2d9b059795b2e5352690dd6aa0ac35
adnanmulalic <adnan.mulalic3@gmail.com>
2022-10-21 12:27:07 +0200

    wip

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ const form = document.querySelector("#form");
 let title = document.querySelector("#title");
 let author = document.querySelector("#author");
 let pages = document.querySelector("#pages");
-let hasRead = document.querySelectorAll("input[name=hasRead]");
+let hasRead = document.querySelector("#read");
 
 function Book (title, author, pages, read) {
     this.title = title;
@@ -21,17 +21,20 @@ function showBooks(books) {
 
 addBookButton.addEventListener("click", () => {
     form.classList.replace("form-hide", "form-display");
+    addBookButton.classList.add("form-hide");
 });
 
 submitButton.addEventListener("click", () => {
-    myLibrary.push(new Book(title.value, author.value, pages.value, hasRead.value));
+    addBookToLibrary();
+    title.value = author.value = pages.value = ""; hasRead.checked = false;
     form.classList.replace("form-display", "form-hide");
+    addBookButton.classList.remove("form-hide");
 })
 
 let myLibrary = [{title: "A song of ice and fire", author: "George R.R. Martin", pages: 500, read: true}];
 
 function addBookToLibrary() {
-    
+    myLibrary.push(new Book(title.value, author.value, pages.value, hasRead.checked));
 }
 
 
